184edce9d404f5a47bfb5a667d20474e8dc0ba4d
Zachary Zirin <50377578+zdzirin@users.noreply.github.com>
2020-03-04 00:16:26 -0500

    finally got the javascript to work

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-function 
+const ROWS = 10;
+const COLS = 2 * ROWS;
+
+function createTiles(container1) {
+    var rowDiv, tileDiv;
+    var tileContainer = document.createElement("div");
+    tileContainer.className = "tileContainer";
+    container1.append(tileContainer);
+    for (int i = 0; i < ROWS; i++) {
+        rowDiv = document.createElement("div");
+        rowDiv.className = "row";
+        rowDiv.id = "r" + i.toString();
+        for (int j = 0; j < COLS; j++) {
+            tileDiv = document.createElement("div");
+            tileDiv.className = "tile";
+            tileDiv.id = rowDiv.id + "t" + j.toString();
+            rowDiv.append(tileDiv);
+        }
+        tileContainer.append(rowDiv);
+    }
+}
+
+window.createTiles(document.getElementById("container1"));
+
